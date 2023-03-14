c03188118ee972c192f40e05f4d34664cbe2e27a
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2023-03-14 17:35:47 -0500

    feat: :sparkles: v-html
    funcion del v-html

--- componentes/my_component.js
+++ componentes/my_component.js
@@ -1,9 +1,9 @@
 export default {
     data(){
         return{
-            basico: "Apolo",
-            intemedio: "Artemis",
-            avanzado: "Sputnik"
+            basico: `<strong style="color:green;">Apolo</strong>`,
+            intemedio:`<strong style="color:orange;">Artemis</strong>`,
+            avanzado: `<strong style="color:blue;">Sputnik</strong>`
         }
     }
 }
