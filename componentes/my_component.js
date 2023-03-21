43fddf28d80bdb112b153f3e6576c5496cc52eb4
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2023-03-21 16:04:38 -0500

    feat: :sparkles: v-if v-else v-else-if
    funcion del v-if v-else v-else-if

--- componentes/my_component.js
+++ componentes/my_component.js
@@ -1,7 +1,11 @@
 export default {
     data(){
         return{
-           centinela:false
+          mensaje:"<em>apolo</em>",
+          mensaje2:"<mark>artemisa</mark",
+          mensaje3:"<u>sputnik</u>",
+          mensaje4:"NO CUMPLE LA CONDICION",
+          opcion:null
         }
     }
 }
