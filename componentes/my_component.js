bbdbd62a7113bdfc22180e100201ba17e835fab0
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2023-03-28 09:03:13 -0500

    feat: :sparkles: v-bind objetos
    funcion del v-bind objetos

--- componentes/my_component.js
+++ componentes/my_component.js
@@ -4,7 +4,10 @@ export default {
           atributo:"src",
             imagen:"https://i.ytimg.com/vi/viszafbV3lg/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWCg9MA8=&rs=AOn4CLDTtFada9kefIl-99Lm-IKgyLzBAQ",
             atributo2:"alt",
-            texto:"mondongo"
+            texto:"mondongo",
+            imagen:{
+                src:"https://i.ytimg.com/vi/viszafbV3lg/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWCg9MA8=&rs=AOn4CLDTtFada9kefIl-99Lm-IKgyLzBAQ",
+                title:"mondongo"
         }
     }
-}
+}}
