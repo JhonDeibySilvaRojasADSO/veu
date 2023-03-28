7a22fbd429fc5726ce0b2418c56009d99067a6f3
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2023-03-28 10:18:02 -0500

    feat: :sparkles: v-bind style
    funcion del  v-bind style

--- componentes/my_component.js
+++ componentes/my_component.js
@@ -1,17 +1,15 @@
 export default {
     data(){
         return{
-          imagen:{
-            src:"https://i.ytimg.com/vi/viszafbV3lg/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWCg9MA8=&rs=AOn4CLDTtFada9kefIl-99Lm-IKgyLzBAQ",
-            title:"mondongo",
-        className:"box",
-        className2:["box","box2"],
-        className3:{
-            box:true,
-            box2:false,
-            box3:true
-        }
-
+            styleName:"background:red",
+            styleName2:["background: #a1a1a1","color:#fff"],
+            styleName3:{
+                background:"violet",
+                color:"#fff",
+                "font-size":"2rem",
+                "font-family":".5rem",
+                margin:".5rem 0"
+            }
     }
-    }
-}}
+}
+}
