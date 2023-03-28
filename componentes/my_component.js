769784cdb9e16d0f55738c66330464024445cb95
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2023-03-28 09:01:09 -0500

    feat: :sparkles: v-bind dinamico
    funcion del v-bind dinamico

--- componentes/my_component.js
+++ componentes/my_component.js
@@ -1,14 +1,10 @@
 export default {
     data(){
         return{
-          cosa:[
-            "uff",
-            "Hola",
-            "No se",
-
-        ],
-        imagen:"https://i.ytimg.com/vi/viszafbV3lg/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWCg9MA8=&rs=AOn4CLDTtFada9kefIl-99Lm-IKgyLzBAQ",
-        texto:"mondongo"
+          atributo:"src",
+            imagen:"https://i.ytimg.com/vi/viszafbV3lg/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWCg9MA8=&rs=AOn4CLDTtFada9kefIl-99Lm-IKgyLzBAQ",
+            atributo2:"alt",
+            texto:"mondongo"
         }
     }
 }
