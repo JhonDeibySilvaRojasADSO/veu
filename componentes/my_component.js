78743d83e0affa7574b35acf9e576a02e8965f77
JhonDeibySilvaRojasADSO <silvarojasjhondeiby@gmail.com>
2023-03-28 09:07:52 -0500

    feat: :sparkles: v-bind class
    funcion del  v-bind class

--- componentes/my_component.js
+++ componentes/my_component.js
@@ -1,13 +1,17 @@
 export default {
     data(){
         return{
-          atributo:"src",
-            imagen:"https://i.ytimg.com/vi/viszafbV3lg/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWCg9MA8=&rs=AOn4CLDTtFada9kefIl-99Lm-IKgyLzBAQ",
-            atributo2:"alt",
-            texto:"mondongo",
-            imagen:{
-                src:"https://i.ytimg.com/vi/viszafbV3lg/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWCg9MA8=&rs=AOn4CLDTtFada9kefIl-99Lm-IKgyLzBAQ",
-                title:"mondongo"
+          imagen:{
+            src:"https://i.ytimg.com/vi/viszafbV3lg/hqdefault.jpg?sqp=-oaymwEmCOADEOgC8quKqQMa8AEB-AH-DoACuAiKAgwIABABGGUgWCg9MA8=&rs=AOn4CLDTtFada9kefIl-99Lm-IKgyLzBAQ",
+            title:"mondongo",
+        className:"box",
+        className2:["box","box2"],
+        className3:{
+            box:true,
+            box2:false,
+            box3:true
         }
+
+    }
     }
 }}
